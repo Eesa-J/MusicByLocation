1c836248da839a0dfe059cef3a8f04084e46ec1d
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-03-03 10:36:35 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Jaswal, Eesa (SPH) on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
